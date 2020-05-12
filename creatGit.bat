1df71b0ed93b6a02a3ecc61296a53e99fe702010
pl168959903 <49907320+pl168959903@users.noreply.github.com>
2020-05-13 01:08:22 +0800

    Delete creatGit.bat

--- creatGit.bat
+++
@@ -1,6 +0,0 @@
-echo "# Altium-CH340" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/pl168959903/Altium-CH340.git
-git push -u origin master
